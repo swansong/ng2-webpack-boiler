379f693ae3334d18a1b668ca5af020270abe3a35
Jon Swanson <jon.swanson@rakuten.com>
2016-05-06 01:12:36 -0700

    added webpack and typescript config

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+var webpack = require("webpack");
+
+//var PROD = ((JSON.parse(process.env['NODE_ENV']) == 'production') || process.argv.indexOf('--production') !== -1);
+//var QA = ((JSON.parse(process.env['NODE_ENV']) == 'qa') || process.argv.indexOf('--qa') !== -1);
+
+var PROD = (process.argv.indexOf('--production') !== -1);
+var QA = (process.argv.indexOf('--qa') !== -1);
+
+var entry = [];
+var devtool = "#source-map";
+var plugins = [];
+var outputFile = "build/app.js";
+
+if (PROD) {
+  entry = './entry/production.ts';
+  devtool = '';
+  plugins = [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: false
+    })
+  ];
+}
+else if (QA) {
+  entry.push('./entry/qa.ts');
+}
+else {
+  entry.push('./entry/development.ts');
+}
+
+module.exports = {
+  entry: entry,
+  devtool: devtool,
+  output: {
+    path: __dirname,
+    filename: outputFile
+  },
+  resolve: {
+    modulesDirectories: ['node_modules', 'bower_components']
+  },
+  module: {
+    loaders: [
+      { test: /\.ts$/, exclude: [/node_modules/, /bower_components/], loader: "ts-loader"},
+      { test: /\.css$/, loader: "text-loader" },
+      { test: /\.less$/, loader: "style!css!less" },
+      { test: /\.(woff|svg|ttf|eot|otf)([\?]?.*)$/, loader: "file-loader?name=fonts/[name].[ext]" },
+      { test: /\.html$/, loader: "text-loader" }
+    ]
+  },
+  plugins: plugins
+};
