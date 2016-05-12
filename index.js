13c2932acb5ce546595214cd43542b8c5a81e38a
Jon Swanson <jon.swanson@rakuten.com>
2016-05-12 16:33:46 -0700

    general cleanup and fix for async actions

--- index.js
+++ index.js
@@ -1,86 +1,85 @@
-module.exports = function() {
-  var config = (process.argv.indexOf('config') !== -1);
-  var boiler = (process.argv.indexOf('boiler') !== -1);
+#!/usr/bin/env node
 
-  var fs = require('fs');
-  var path = require('path');
+var config = (process.argv.indexOf('--config') !== -1);
+var boiler = (process.argv.indexOf('--boiler') !== -1);
 
-  var parentRoot = path.resolve(__dirname).split('/node_modules')[0] + '/';
-  var thisRoot = path.resolve(__dirname) + '/';
-  
-  if (boiler) {
-    return hardCopyAll();
+var fs = require('fs');
+var path = require('path');
+var ncp = require('ncp').ncp;
+
+var parentRoot = path.resolve(__dirname).split('/node_modules')[0] + '/';
+var thisRoot = path.resolve(__dirname) + '/';
+
+if (config) {
+  copySampleConfig();
+}
+else if (boiler) {
+  copyBoilerplateBuild();
+}
+else {
+  printHelp();
+}
+
+function printHelp() {
+  console.log('This utility helps keep your webpack build up to date and provides an Angular app boilerplate to start from. It recongnizes two arguments: "--config" and "--boiler"');
+  console.log('"ng2-webpack --boiler" will install the app and build boilerplate code into your current directory');
+  console.log('"ng2-webpack --config" will create/update a sample.webpack.config.js file with the latest build goodness so you can see what is new');
+}
+
+function copySampleConfig() {
+  console.log('copying sample webpack config for comparison purposes');
+  fs.createReadStream(thisRoot + 'webpack.config.js').pipe(fs.createWriteStream(parentRoot + 'sample.webpack.config.js'));
+}
+
+function copyBoilerplateBuild() {
+  var locations = ['entry', 'config', 'app', 'webpack.config.js', 'shims.d.ts', 'tsconfig.json'];
+  var test = false;
+  var index = 0;
+
+  console.log('preparing to copy webpack config and app boilerplate into your project');
+
+  while (index < locations.length && !test) {
+    test = checkIfExists(parentRoot + locations[index]);
+    index++;
   }
 
-  if (config) {
-    return copySampleConfig();
+  if (test instanceof Error) {
+    console.log('something went wrong, aborting. Error: ', test);
   }
-
-  return printHelp();
-
-  function printHelp() {
-    process.stdout.write('This utility keeps your webpack build up to date and provides an Angular app boilerplate to start from.');
-    process.stdout.write('It accepts two arguments: "config" and "boiler"');
-    process.stdout.write('"ng2-webpack boiler" will install the app and build boilerplate code into your current directory');
-    process.stdout.write('"ng2-webpack config" will create/update a sample.webpack.config.js file with the latest build goodness so you can see what is new');
-    return;
+  else if (test) {
+    process.stdout.write('this is a hard copy and will overwrite your app, entry, and config directories, as well as your webpack.config.js, shims.d.ts and tsconfig.json files. Continue? (y/n) ');
+    process.stdin.once('data', function (buffer) {
+      var text = buffer.toString().trim().toLowerCase();
+      if (text === 'y' || text === 'yes') {
+        hardCopyAll(locations, process.exit);
+      }
+      else {
+        console.log('aborting');
+        process.exit();
+      }
+    });
   }
-
-  function copySampleConfig() {
-    process.stdout.write('copying sample webpack config for comparison purposes');
-    fs.createReadStream(thisRoot + 'webpack.config.js').pipe(fs.createWriteStream(parentRoot + 'sample.webpack.config.js'));
+  else {
+    hardCopyAll(locations);
   }
+}
 
-  function copyBoilerplateBuild() {
-    var locations = ['entry', 'config', 'app', 'webpack.config.js', 'shims.d.ts', 'tsconfig.json'];
-    var test = false;
-    var index = 0;
-
-    process.stdout.write('preparing to copy webpack config and app boilerplate into your project');
-
-    while (index < locations.length && !test) {
-      test = checkIfExists(parentRoot + location[index]);
-      index++;
-    }
-
-    if (test instanceof Error) {
-      process.stdout.write('something went wrong, aborting. Error: ', test);
-    }
-    else if (test) {
-      process.stdout.write('this is a hard copy and will overwrite your app, entry, and config directories, as well as your webpack.config.js, shims.d.ts and tsconfig.json files. Continue? y/n');
-      process.stdin.once('data', function (text) {
-        text = text.trim();
-        if (text == 'y' || text == 'Y' || text == 'yes' || text == 'Yes') {
-          hardCopyAll(locations);
-        }
-        else {
-          process.stdout.write('aborting');
-        }
-      });
-    }
-    else {
-      hardCopyAll(locations);
-    }
+function checkIfExists(fullPath) {
+  try {
+    fs.lstatSync(fullPath);
+    return true;
   }
-
-  function checkIfExists(fullPath) {
-    try {
-      fs.lstatSync(fullPath);
-      return true;
-    }
-    catch (err) {
-      if (err.code == 'ENOENT') return false;
-      return err;
-    }
+  catch (err) {
+    if (err.code == 'ENOENT') return false;
+    return err;
   }
-  
-  function hardCopyAll() {
-    var ncp = require('ncp').ncp;
+}
 
-    for (var i = 0; i < locations.length; i++) {  
-      ncp(thisRoot + locations[i], parentRoot + locations[i], function (err) {
-        if (err) process.stdout.write(err);
-      }); 
-    }
-  }
-};
+function hardCopyAll(locations, callback) {
+  if (!locations.length) callback();
+  var current = locations.shift();
+  ncp(thisRoot + current, parentRoot + current, function (err) {
+    if (err) console.log('error copying file ' + current, err);
+    hardCopyAll(locations, callback);
+  }); 
+}
